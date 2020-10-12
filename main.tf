d25ef20bf12675bb30fcc427c20af1620d3915a7
Lahindu Weerarathna <Lahindu_105097@dialog.dialoggsm.com>
2020-10-12 10:40:36 +0530

    terraform test 1

--- main.tf
+++ main.tf
@@ -24,40 +24,40 @@ module "vpc" {
     private_subnet_cidr_blocks  = var.private_subnet_cidr_blocks
 }
 
-#module "eks" {
-#    source                      = "./modules/eks"
-#    eks_cluster_name            = var.eks_cluster_name
-#    environment                 = var.environment
-#    eks_cluster_role            = "prod_eksClusterRole"
-#    subnet_id_in_vpc            = [ module.vpc.public_subnet_ids[0], module.vpc.public_subnet_ids[1], module.vpc.private_subnet_ids[0], module.vpc.private_subnet_ids[1]]
-#    cluster_sg_name             = "${var.eks_cluster_name}-SG"
-#    vpc_id                      = module.vpc.vpc_id
-#    nodes_sg_name               = "${var.eks_cluster_name}-NG-SG"
-#    public_subnet_ids           = [ module.vpc.public_subnet_ids[0], module.vpc.public_subnet_ids[1]]
-#    private_subnet_ids          = [ module.vpc.private_subnet_ids[0], module.vpc.private_subnet_ids[1]]
-#    ami_type                    = "AL2_x86_64"
-#    disk_size                   = 10
-#    instance_types              = ["t3.small"]
-#    asg_public_desired_size     = 1
-#    asg_public_max_size         = 1
-#    asg_public_min_size         = 1
-#    asg_private_desired_size    = 2
-#    asg_private_max_size        = 2
-#    asg_private_min_size        = 2
-#}
-
-module "ec2"{
-    source                      = "./modules/ec2"
-    vpc_id                      = module.vpc.vpc_id
-    instance_name               = "jenkins_server"
-    instance_public_key         = var.instance_public_key
+module "eks" {
+    source                      = "./modules/eks"
+    eks_cluster_name            = var.eks_cluster_name
     environment                 = var.environment
-    instance_ami                = "ami-093da183b859d5a4b"
-    instance_type               = "t2.micro"
-    subnet_id                   = module.vpc.public_subnet_ids[0]
-    private_ip                  = "10.1.0.4"
-    ec2_eks_cluster_role        = "EKS-ACCESS-TO-EC2-ROLE"
+    eks_cluster_role            = "prod_eksClusterRole"
+    subnet_id_in_vpc            = [ module.vpc.public_subnet_ids[0], module.vpc.public_subnet_ids[1], module.vpc.private_subnet_ids[0], module.vpc.private_subnet_ids[1]]
+    cluster_sg_name             = "${var.eks_cluster_name}-SG"
+    vpc_id                      = module.vpc.vpc_id
+    nodes_sg_name               = "${var.eks_cluster_name}-NG-SG"
+    public_subnet_ids           = [ module.vpc.public_subnet_ids[0], module.vpc.public_subnet_ids[1]]
+    private_subnet_ids          = [ module.vpc.private_subnet_ids[0], module.vpc.private_subnet_ids[1]]
+    ami_type                    = "AL2_x86_64"
+    disk_size                   = 10
+    instance_types              = ["t3.small"]
+    asg_public_desired_size     = 1
+    asg_public_max_size         = 1
+    asg_public_min_size         = 1
+    asg_private_desired_size    = 2
+    asg_private_max_size        = 2
+    asg_private_min_size        = 2
 }
+
+#module "ec2"{
+#    source                      = "./modules/ec2"
+#    vpc_id                      = module.vpc.vpc_id
+#    instance_name               = "jenkins_server"
+#    instance_public_key         = var.instance_public_key
+#    environment                 = var.environment
+#    instance_ami                = "ami-093da183b859d5a4b"
+#    instance_type               = "t2.micro"
+#    subnet_id                   = module.vpc.public_subnet_ids[0]
+#    private_ip                  = "10.1.0.4"
+#    ec2_eks_cluster_role        = "EKS-ACCESS-TO-EC2-ROLE"
+#}
 
 #module "ecr"{
 #    source                      = "./modules/ecr"
